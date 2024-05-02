034cb8608a2f460f610a71d630c5ff03fe8c16c9
mirmakhmudov16 <mr.mirmakhmudov16112008@gmail.com>
2024-05-02 21:41:44 +0500

    Commit

--- apps/admin.py
+++ apps/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
 from apps.models import Product, Post
+from django import forms
 
-admin.site.register(Product)
-admin.site.register(Post)
+
+class PostAdminForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = "__all__"
+
+    def clean_title(self):
+        if self.cleaned_data["title"] == "Zombie":
+            raise forms.ValidationError("No Vampires")
+
+        return self.cleaned_data["title"]
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "time", "description")
+    search_fields = ("title__startswith",)
+    list_filter = ("title", "time", "description",)
+    form = PostAdminForm
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "title")
+    search_fields = ("name__startswith",)
+    list_filter = ("name", "price", "title",)
